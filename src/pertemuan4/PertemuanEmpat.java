fc452fb84756b58c498b4ba66bb713597bc97980
Iman Muliawan <imanmuliawan@gmail.com>
2017-07-25 22:03:47 +0700

    Tugas Tiga Pertemuan4 - commit 3

---
+++ src/pertemuan4/PertemuanEmpat.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan4;
+
+/**
+ *
+ * @author iman
+ */
+public class PertemuanEmpat {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PertemuanEmpatForm f = new PertemuanEmpatForm();
+        f.setVisible(true);
+    }
+}
